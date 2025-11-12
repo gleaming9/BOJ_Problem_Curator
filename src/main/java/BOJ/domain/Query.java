3bd9d3581899097f75b77e177847f0deca52c507
gleaming9 <gleaming9@khu.ac.kr>
2025-11-12 19:36:17 +0900

    feat: Query 객체 구현
    Query는 사용자의 입력을 바탕으로 solved.ac api 요청 시 알맞은 형태의 쿼리 스트링으로 변환합니다.

---
+++ src/main/java/BOJ/domain/Query.java
@@ -0,0 +1,34 @@
+package BOJ.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Query {
+    private final String tag;
+    private final String minimumTier;
+    private final String maximumTier;
+    private final Integer solvedCount;
+    private final Integer isKorean;
+
+    public String toQueryString() {
+        StringBuilder queryBuilder = new StringBuilder();
+        if(tag != null && !tag.isBlank()) {
+            String[] tags = tag.split(",");
+            for (String tagValue : tags) {
+                queryBuilder.append("tag:").append(tag).append(" ");
+            }
+        }
+        if(!(minimumTier.isBlank() && maximumTier.isBlank())) {
+            queryBuilder.append("*").append(minimumTier).append("..").append(maximumTier).append(" ");
+        }
+        if(solvedCount != null) {
+            queryBuilder.append("s#").append(solvedCount).append(".. ");
+        }
+        if(isKorean != null){
+            queryBuilder.append("%ko");
+        }
+        return queryBuilder.toString();
+    }
+}
