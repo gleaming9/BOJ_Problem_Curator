857d62df61ea9705ed1665d2b34a3a26e1f3b3e6
gleaming9 <gleaming9@khu.ac.kr>
2025-11-12 19:40:27 +0900

    feat: 사용자 입력 기능 세분화
     난이도 입력을 최소 난이도, 최대 난이도 입력으로 분리했습니다.
    
     한국어 문제 여부를 추가했습니다.

--- src/main/java/BOJ/view/InputView.java
+++ src/main/java/BOJ/view/InputView.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String TAGS_MESSAGE = "알고리즘 태그 입력(예: \"dp\", \"dp,greedy\") : ";
-    private static final String DIFFICULTY_MESSAGE = "난이도 입력(예: g5, s1..g5) : ";
+    private static final String MINIMUM_TIER_MESSAGE = "최소 난이도 입력 : ";
+    private static final String MAXIMUM_TIER_MESSAGE = "최대 난이도 입력 : ";
     private static final String SOLVED_COUNT_MESSAGE = "최소 푼 사람 수 (예: 1000) : ";
+    private static final String KOREAN_MESSAGE = "한국어 문제만 볼 건지 여부 (예: 0, 1) : ";
 
     private static final Scanner sc = new Scanner(System.in);
 
@@ -14,13 +16,23 @@ public class InputView {
         return sc.next();
     }
 
-    public static String getDifficulty(){
-        System.out.println(DIFFICULTY_MESSAGE);
+    public static String getMinTier(){
+        System.out.println(MINIMUM_TIER_MESSAGE);
+        return sc.next();
+    }
+
+    public static String getMaxTier(){
+        System.out.println(MAXIMUM_TIER_MESSAGE);
         return sc.next();
     }
 
     public static int getSolvedCount(){
         System.out.println(SOLVED_COUNT_MESSAGE);
+        return sc.nextInt();
+    }
+
+    public static int getKorean(){
+        System.out.println(KOREAN_MESSAGE);
         return sc.nextInt();
     }
 }
