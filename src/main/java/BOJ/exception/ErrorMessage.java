6db396348d44f7502bdbae3f34921314d99602b0
gleaming9 <gleaming9@khu.ac.kr>
2025-11-13 23:45:25 +0900

    feat: API 통신 및 JSON 파싱 에러메세지 추가

--- src/main/java/BOJ/exception/ErrorMessage.java
+++ src/main/java/BOJ/exception/ErrorMessage.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 @Getter
 public enum ErrorMessage {
     INVALID_TAG_NAME("태그는 영문자, 언더스코어, 공백만 허용됩니다."),
-    INVALID_TIER_NAME("올바르지 않은 티어 형식입니다. (가능한 티어 형식 : g5, s1, p3)");
+    INVALID_TIER_NAME("올바르지 않은 티어 형식입니다. (가능한 티어 형식 : g5, s1, p3)"),
+
+    API_CONNECTION_ERROR("Solved.ac API 통신 중 오류가 발생했습니다."),
+    API_REQUEST_FAILED("API 요청에 실패했습니다. 상태 코드 : "),
+
+    JSON_PARSING_FAILED("API 응답 데이터를 분석하는 데 실패했습니다.");
 
     private final String message;
 
