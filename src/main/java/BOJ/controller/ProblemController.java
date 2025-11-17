a5793280670a68622b5534b647a4568838b7a8b2
gleaming9 <gleaming9@khu.ac.kr>
2025-11-17 17:25:27 +0900

    refactor: ResponseEntity 적용
    기존의 응답 형식은 List<Problem>을 JSON으로 던지는 방식이었는데, ResponseEntity를 사용해서 statusCode도 올바르게 포함된 JSON을 반환하도록 변경합니다.

--- src/main/java/BOJ/controller/ProblemController.java
+++ src/main/java/BOJ/controller/ProblemController.java
@@ -3,8 +3,8 @@ package BOJ.controller;
 import BOJ.domain.Problem;
 import BOJ.dto.SearchRequest;
 import BOJ.service.ProblemService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,7 +18,8 @@ public class ProblemController {
     }
 
     @GetMapping("api/search")
-    public List<Problem> searchProblems(SearchRequest request){
-        return problemService.search(request);
+    public ResponseEntity<List<Problem>> searchProblems(SearchRequest request){
+        List<Problem> problems = problemService.search(request);
+        return ResponseEntity.ok(problems);
     }
 }
